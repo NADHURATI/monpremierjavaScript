7c973926b6092380fecf7bceab7293f3e1ba3dff
NADHURATI <nadhuratiali47@gmail.com>
2024-10-17 16:10:10 +0300

    ajout dom.js index.html

--- dom.js
+++ dom.js
@@ -21,14 +21,58 @@ let monAgent = document.querySelector("#agent1");
 console.log(monAgent);
 
 let maListe = document.querySelector(" .maListe");
+// Récuperer plusieurs éléments appartenant à la classe. ma
 console.log(" Ma Deuxième liste", maListe);
+
 
 let maListeIntegrale = document.querySelectorAll(".maListe");
 console.log("Ma liste Intégrale: ", maListeIntegrale);
 
+ for(let i = 0; i < maListeIntegrale.length; i++) {
+    console.log(maListeIntegrale[i]);
+ }
+
 let monParagraphe = document.querySelector("#paragraphe1");
 console.log(" Mon paragraphe" , monParagraphe)
 
+// Modifier un élément du DOM
+
+let elementImage = document.querySelector("#premiereImage");
+elementImage.setAttribute("class", "photoModifiee");
+
+elementImage.setAttribute("alt", "Mon image doit s'afficher" );
+console.log("Mon image Avant de modification :", elementImage );
+
+elementImage.classListe.add("nouvelleClasse");
+console.log("ajout nouvelle classe:", elementImage);
+
+elementImage.classList.remove("photo");
+
+// Créer un nouvel élément dans la page
+// on utilise la méthode createElement()
+let nouvelDiv = document.createElement("div");
+let nouveauTitre = document.createElement("h3");
+
+// <h3> Mon titre de section niveau 3 </h3>
+nouveauTitre.textContent = "Mon titre de section niveau 3";
+
+// Sélectionner l'élément body
+let body = document.querySelector('body');
+body.appendChild(nouveauDiv);
+
+let monDiv = document.querySelector("div");
+monDiv.appendChild(nouveauTitre);
+
+/* <body>
+        <div> => body.apprendChild(nouveauDiv);
+            <h3> Mon titre de section niveau 3 </h3>
+        
+        
+        </div>
+
+</body>
+
+*/
 
 
 
